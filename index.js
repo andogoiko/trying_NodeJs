e63b4b6e6bcebf8f00a97acaf21857e16dd068d6
Ando <andoitz.goico@gmail.com>
2023-11-19 13:51:33 +0100

    creando servidor

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const fs = require('fs')
+const http = require('http');
 
-fs.readFile('./texto.txt', function (err, data){
-    if(err){
-        console.log(err);
-    }
-    console.log(data.toString())
+const handleServer = function (req, res){
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.write('<h1>Hola mundo desde NodeJs</h1>');
+    res.end();
+};
+
+const server = http.createServer(handleServer);
+
+server.listen(3000, function(){
+    console.log('server on port 3000')
 });
-
-console.log
---
+++ index_readfile.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+fs.readFile('./texto.txt', function (err, data){
+    if(err){
+        console.log(err);
+    }
+    console.log(data.toString())
+});
+
+console.log
